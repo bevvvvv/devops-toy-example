50d39c148cba7f707c26f5dc657c8da8bf5f9954
bevvvvv <computerwhiz1337@gmail.com>
2022-01-29 12:59:47 -0500

    successful test suite

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,68 @@
 #!/bin/sh
 
 # uses generated key for ease of use
-PASSWORD=$(openssl aes-256-cbc -a -in secret.txt -pass file:/key/secret.key)
+PASSWORD=$(echo -n $PASS | openssl enc -e -aes-256-cbc -a -pass file:/key/secret.key)
 
 
-curl --location --request POST 'http://webhook:9000/hooks/write' \
---header 'File-Name: test.txt' \
+# remove existing test files
+rm /var/lib/webhook-data/foo.txt
+rm /var/lib/webhook-data/bar.txt
+
+
+curl -s --location --request POST 'http://webhook:9000/hooks/write' \
+--header 'File-Name: foo.txt' \
 --header 'Secret: '$PASSWORD'' \
 --header 'Content-Type: application/json' \
---data-raw '{ "text": "number 66 and number 2\n and number 3" }'
+--data-raw '{ "text": "number 1 and number 2 and number 3" }'
+
+EXPECTED="number 1 and number 2 and number 3"
+RESULT="$(cat /var/lib/webhook-data/foo.txt)"
+if [ "$EXPECTED" != "$RESULT" ]
+then 
+    echo "First test case failed"
+    exit 1
+fi
+
+curl -s --location --request POST 'http://webhook:9000/hooks/write' \
+--header 'File-Name: foo.txt' \
+--header 'Secret: '$PASSWORD'' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "text": "number 2 and number 3 and number 4" }'
+
+EXPECTED="number 1 and number 2 and number 3 number 2 and number 3 and number 4"
+RESULT="$(echo -e $(cat /var/lib/webhook-data/foo.txt))"
+if [ "$EXPECTED" != "$RESULT" ]
+then 
+    echo "Second test case failed"
+    exit 1
+fi
+
+curl -s --location --request POST 'http://webhook:9000/hooks/write' \
+--header 'File-Name: bar.txt' \
+--header 'Secret: '$PASSWORD'' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "text": "number 4 and number 5 and number 6" }'
+
+EXPECTED="number 4 and number 5 and number 6"
+RESULT="$(cat /var/lib/webhook-data/bar.txt)"
+if [ "$EXPECTED" != "$RESULT" ]
+then 
+    echo "Third test case failed"
+    exit 1
+fi
+
+# TEST AUTHENTICATION FAILS to execute
+PASSWORD=$(echo -n "failure" | openssl enc -e -aes-256-cbc -a -pass file:/key/secret.key)
+curl -s --location --request POST 'http://webhook:9000/hooks/write' \
+--header 'File-Name: bar.txt' \
+--header 'Secret: '$PASSWORD'' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "text": "number 4 and number 5 and number 6" }'
+
+EXPECTED="number 4 and number 5 and number 6"
+RESULT="$(cat /var/lib/webhook-data/bar.txt)"
+if [ "$EXPECTED" != "$RESULT" ]
+then 
+    echo "Last test case failed"
+    exit 1
+fi
--- webhook/write-webhook.sh
+++ webhook/write-webhook.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+set -e
+PASSWORD=$(echo $3| openssl base64 -d | openssl enc -d -aes-256-cbc -pass file:/secret.key)
+
+if [ $PASSWORD != $PASS ]
+then
+    echo "Permission Denied."
+    exit 126
+fi
+
 echo -e $1 >> $2
